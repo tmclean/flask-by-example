e4da5b0714a422b5392d46d0036a532c7275ca7d
tmclean <5218332+tmclean@users.noreply.github.com>
2021-01-03 20:22:31 -0500

    Added configs

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
+import os
 from flask import Flask
 
 app = Flask( __name__ )
+app.config.from_object( os.environ['APP_SETTINGS'] )
 
 @app.route( "/" )
 def hello():
@@ -11,6 +13,7 @@ def hello_name( name ):
    return "Hello, {}!".format( name )
 
 if __name__ == '__main__':
+   print( os.environ['APP_SETTINGS'] )
    app.run()
 
 
